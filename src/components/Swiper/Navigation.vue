fcc9da820515328457e260ee68d1c3d0d8925774
NahlaGalal <nahlaglal@gmail.com>
2023-03-31 06:27:58 +0200

    Add Horizontal swiper components

---
+++ src/components/Swiper/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="swiper.slidePrev()" v-if="navigationType === 'prev'">
+    <img src="../../assets/images/prev.svg" alt="Prev icon">
+  </button>
+
+  <button @click="swiper.slideNext()" v-if="navigationType === 'next'">
+    <img src="../../assets/images/next.svg" alt="Next icon">
+  </button>
+</template>
+
+<script lang="ts">
+import { useSwiper } from "swiper/vue"
+
+export default {
+  props: {
+    navigationType: String
+  },
+  setup() {
+    const swiper = useSwiper();
+
+    return { swiper };
+  },
+}
+</script>
