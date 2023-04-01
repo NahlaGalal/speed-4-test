0dda6f056bd15ac4ec1add63b10781741d24a09f
NahlaGalal <nahlaglal@gmail.com>
2023-04-01 08:54:58 +0200

    Add button component

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- If to prop is undefined then it is a regular button -->
+  <button :type="type" v-if="!to" :class="`[ py-3 px-5 sm:px-24 ] 
+  [ w-full sm:w-max ] 
+  [ rounded-full bg-[#6c98a2] ] 
+  [ text-white font-semibold text-3xl ] ` + classNames" 
+  @click="onClick">{{ text }}</button>
+
+  <!-- If to prop is not undefined then it is a link -->
+  <RouterLink :to="to" v-else :class="`[ py-3 px-5 sm:px-24 ]
+    [ w-full sm:w-max ]
+    [ rounded-full bg-[#6c98a2] ] 
+    [ text-white font-semibold text-3xl ] ` + classNames"
+    >
+    {{ text }}
+  </RouterLink>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue';
+
+export default {
+  props: {
+    type: {
+      type: String as PropType<"button" | "reset" | "submit">,
+      default: "button",
+      validator(value: string) {
+        // The value must match one of these strings
+        return ['button', 'reset', 'submit'].includes(value)
+      }
+    },
+    text: String,
+    classNames: String,
+    to: String,
+    onClick: Function as PropType<(payload: MouseEvent) => void>
+  }
+}
+</script>
