837b8bf2a6f7b6755c0600d8eb87be2e3a266cc9
NahlaGalal <nahlaglal@gmail.com>
2023-04-01 08:55:08 +0200

    Add input component

---
+++ src/components/FormControl/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label :for="id" class="text-lg font-medium leading-6 mb-1 text-[#958a7e] block text-right">{{ label }}</label>
+    <input :type="type" :placeholder="placeholder" :id="id" :name="name"
+      class="[ border-0 border-b borer-b-[#DDD] ]
+      [ text-xl font-medium leading-7 ]
+      [ text-black placeholder:text-black ]
+      outline-none w-full pb-1 ">
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    type: String,
+    placeholder: String,
+    id: String,
+    name: String,
+    label: String
+  }
+}
+</script>
