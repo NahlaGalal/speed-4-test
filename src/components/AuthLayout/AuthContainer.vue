d8c636643a36746d90ad1fa9b659af139fab559a
NahlaGalal <nahlaglal@gmail.com>
2023-04-01 08:12:48 +0200

    Split auth container to a component

---
+++ src/components/AuthLayout/AuthContainer.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="flex flex-col justify-center items-center h-screen z-10 relative px-5">
+    <!-- Logo => if logo is outside the white section -->
+    <img src="../../assets/images/logo.svg" alt="Logo" v-if="isLogoOut" class="mb-20">
+
+    <section class="bg-white text-center w-max rounded-[60px] 
+      [ py-12 px-6 sm:px-[86px] ]
+      [ flex flex-col justify-center items-center ]
+      [ text-3xl font-medium text-[#958a7e] ]">
+
+      <!-- Logo => if logo is inside the white section -->
+      <img src="../../assets/images/logo.svg" alt="Logo" v-if="!isLogoOut" class="mb-16">
+
+      <!-- Header -->
+      <h1 class="text-5xl font-bold mb-5 text-black">{{ header }}</h1>
+
+      <!-- Sub header -->
+      <p class="leading-5 mb-12">{{ subHeader }}</p>
+
+      <!-- Rest of component -->
+      <slot name="section" />
+    </section>
+
+    <!-- If there is a text under white section -->
+    <slot name="text-bottom" />
+  </main>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    isLogoOut: Boolean,
+    header: String,
+    subHeader: String,
+  }
+}
+</script>
