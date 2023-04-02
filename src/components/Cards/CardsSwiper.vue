1c10f637dd44a37cbce019ac8ed92087b9b33917
NahlaGalal <nahlaglal@gmail.com>
2023-04-02 08:50:17 +0200

    Remove warning

--- src/components/Cards/CardsSwiper.vue
+++ src/components/Cards/CardsSwiper.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script lang="ts">
-import { SwiperSlide, useSwiper } from "swiper/vue"
+import { SwiperSlide } from "swiper/vue"
 import { PropType } from 'vue';
 import { IProduct } from '../../Types';
 import Card from './Card.vue';
@@ -16,11 +16,6 @@ import HorizontalSwiper from '../Swiper/HorizontalSwiper.vue';
 export default {
   emits: ['toggleFavourites'],
   components: { Card, HorizontalSwiper, SwiperSlide },
-  setup() {
-    const swiper = useSwiper();
-
-    return { swiper };
-  },
   props: {
     products: Array as PropType<IProduct[]>,
   }
