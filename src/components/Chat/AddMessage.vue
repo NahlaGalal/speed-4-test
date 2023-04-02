f151949450dd88a406813e9e4c23b0f7619116ff
NahlaGalal <nahlaglal@gmail.com>
2023-04-02 07:48:11 +0200

    Edit add message functionality

--- src/components/Chat/AddMessage.vue
+++ src/components/Chat/AddMessage.vue
@@ -1,17 +1,18 @@
 <template>
   <Form class="[ flex flex-wrap items-center gap-4 ] 
-    [ w-4/5 xl:w-1/2 ]
-    [ mx-auto mt-44 ]" :validation-schema="schema" @submit="$emit('handleSubmit', $event)" :initial-values="defaults">
+              [ w-4/5 xl:w-1/2 ]
+              [ mx-auto mt-44 ]" :validation-schema="schema" @submit="onSubmitHandler($event)"
+    :initial-values="defaults">
     <button title="Send Message" class="[ w-24 h-24 ] 
-        [ rounded-full bg-[#6c98a2] ]
-        [ flex justify-center items-center ]">
+                  [ rounded-full bg-[#6c98a2] ]
+                  [ flex justify-center items-center ]">
       <img src="../../assets/images/send.svg" alt="Send icon">
     </button>
 
     <Field as="textarea" name="message" class="[ h-[104px] flex-1 ] resize-none
-        [ rounded-full bg-[#f7f5ef] ] 
-        [ text-xl pr-10 py-4 ]
-        [ border border-[#43290A] border-opacity-5 ]"></Field>
+                  [ rounded-full bg-[#f7f5ef] ] 
+                  [ text-xl pr-10 py-4 ]
+                  [ border border-[#43290A] border-opacity-5 ]" v-model="message"></Field>
     <ErrorMessage name="message" class="w-full text-base text-[#fa4248] block text-right" />
   </Form>
 </template>
@@ -28,6 +29,17 @@ export default {
     schema: Object,
     apiError: String,
     defaults: Object as PropType<ISendMessageFormTypes>
+  },
+  data() {
+    return {
+      message: ""
+    }
+  },
+  methods: {
+    onSubmitHandler(event: any) {
+      this.$emit('handleSubmit', event)
+      this.message = ''
+    }
   }
 }
 </script>
--- src/components/Chat/MsgsContainer.vue
+++ src/components/Chat/MsgsContainer.vue
@@ -1,6 +1,8 @@
 <template>
-  <main class="w-4/5 mx-auto flex flex-col gap-10">
-    <Message v-for="msg in messages" :key="msg.message_id" :message="msg" />
+  <main class="w-4/5 mx-auto flex flex-col gap-10
+      max-h-[350px] overflow-auto" ref="messages">
+    <!-- Loop for message and reverse them to show the newest message in the bottom -->
+    <Message v-for="msg in messages.slice().reverse()" :key="msg.message_id" :message="msg" />
   </main>
 </template>
 
@@ -16,6 +18,11 @@ export default {
       required: true
     }
   },
-  components: { Message }
+  components: { Message },
+  updated() {
+    // Scroll to the bottom to the last message
+    const messageDisplay: any = this.$refs.messages;
+    messageDisplay.scrollTop = messageDisplay.scrollHeight;
+  },
 }
 </script>
