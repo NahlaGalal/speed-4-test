b67edb26c484a8899ee20ec4465db7ebf93f3e85
NahlaGalal <nahlaglal@gmail.com>
2023-04-02 10:05:10 +0200

    Add Error page design

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <img src="../assets/images/error.svg" alt="Error icon">
-    <p>404</p>
-  </div>
+  <main class="custom-container my-10">
+    <img src="../assets/images/error.svg" alt="Error icon" class="w-3/5 mx-auto">
+    <h1 className="text-4xl text-errorColor font-bold text-center my-10">
+      Page Not Found
+    </h1>
+    <Button to="/" text="Go Home" classNames="block mx-auto" />
+  </main>
 </template>
 
 <script lang="ts">
+import Button from '../components/Button/Button.vue';
+
 export default {
-  
+    components: { Button }
 }
 </script>
